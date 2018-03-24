04816d454eaf382d79bed260e827c4a11c8efaa2
Eiichi Sato <sato.eiichi@gmail.com>
2018-03-24 16:17:50 +0900

    .

--- ip_balancer.c
+++ ip_balancer.c
@@ -73,17 +73,16 @@ static int __init ip_balancer_init(void)
 	}
 	pr_info("located connect(2) = %px\n", orig_connect);
 
-	ip_balancer_override_sys_call(__NR_connect, (sys_call_ptr_t) ip_balancer_connect);
-
-	return 0;
+	return ip_balancer_override_sys_call(__NR_connect, (sys_call_ptr_t) ip_balancer_connect);
 }
 
 
 static void __exit ip_balancer_exit(void)
 {
-	ip_balancer_override_sys_call(__NR_connect, (sys_call_ptr_t) orig_connect);
-	pr_info("exit\n");
+	int err = ip_balancer_override_sys_call(__NR_connect, (sys_call_ptr_t) orig_connect);
+	WARN_ON(err);
 
+	pr_info("exit\n");
 	return;
 }
 
