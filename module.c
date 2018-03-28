b88c869a9a0cd0cab2c99b6c3344af2829260405
Eiichi Sato <sato.eiichi@gmail.com>
2018-03-28 23:24:35 +0900

    .

--- module.c
+++ module.c
@@ -6,19 +6,31 @@
 #include "module-test.h"
 #include "module-netns.h"
 #include "module-syscall.h"
+#include "module-netlink.h"
 
 
 static int __init clb_init(void)
 {
     pr_info("init\n");
+
     clb_module_test();
+
+    int err;
     clb_module_netns_init();
-    return clb_module_syscall_init();
+    err = clb_module_syscall_init();
+    if (err)
+        goto fail_syscall_init;
+    clb_module_netlink_init();
+    return 0;
+fail_syscall_init:
+    clb_module_netns_exit();
+    return err;
 }
 
 
 static void __exit clb_exit(void)
 {
+    clb_module_netlink_exit();
     clb_module_syscall_exit();
     clb_module_netns_exit(); // FIXME: Possible use-after-free. Some clients may still be running our connect(2) handler even after syscall_exit() returned.
     pr_info("exit\n");
